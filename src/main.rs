62c50be59b9e58a028bd6c4a386f9d33fea6ee62
hrkrshnn <git@hrkrshnn.com>
2023-04-14 16:50:26 +0200

    feat: can deserialize the structs and output
    Closes: https://github.com/hrkrshnn/solsearch/issues/2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, solsearch!");
+use clap::{Parser, Subcommand};
+use eyre::Result;
+use solidity::ast::SourceUnit;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    pub sub: Subcommands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Subcommands {
+    #[clap(name = "pretty-print")]
+    PrettyPrint {
+        #[clap(help = "A JSON file to solc's AST")]
+        path: String,
+    },
+}
+
+fn main() -> Result<()> {
+    let opts = Cli::parse();
+    println!("{opts:?}");
+
+    match opts.sub {
+        Subcommands::PrettyPrint { path } => pretty_print(path)?,
+    }
+
+    Ok(())
+}
+
+// TODO move into a separate file later
+fn pretty_print(path: String) -> Result<()> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let deserialized_data: SourceUnit = serde_json::from_reader(reader)?;
+    println!("{deserialized_data:#?}");
+    Ok(())
 }
